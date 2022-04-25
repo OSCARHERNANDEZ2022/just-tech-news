3cc48de76de975cc880f09f82af4790e93fb10b3
Oscar Hernandez <oscarhernandezmex@gmail.com>
2022-04-25 16:23:27 -0700

    end of 13.5.7

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -7,6 +7,7 @@ router.get('/', (req, res) => {
     attributes: { exclude: ['password'] }
   })
     .then(dbUserData => res.json(dbUserData))
+    .then(data => res.send(data))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
@@ -45,6 +46,29 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+  User.findOne({
+    where: {
+      email: req.body.email
+    }
+  }).then(dbUserData => {
+    if (!dbUserData) {
+      res.status(400).json({ message: 'No user with that email address!' });
+      return;
+    }
+
+    const validPassword = dbUserData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res.status(400).json({ message: 'Incorrect password!' });
+      return;
+    }
+
+    res.json({ user: dbUserData, message: 'You are now logged in!' });
+  });
 });
 
 router.put('/:id', (req, res) => {
@@ -88,6 +112,5 @@ router.delete('/:id', (req, res) => {
       res.status(500).json(err);
     });
 });
-
 
 module.exports = router;
